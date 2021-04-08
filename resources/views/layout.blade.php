340b94103c9778c75da69fdb2a260bfe168d5f14
Maira Yicela Rengifo <mairarengifo143@gmail.com>
2021-04-08 17:53:02 -0500

    Se Agrega la funcionalidad de mostrar lista de libros y de crear libro

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,11 +17,11 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="{{ route('libros.index')">Lista de Libros</a>
+          <a class="nav-link" href="{{ route('libros.index')}}">Lista de Libros</a>
       </li>
 
       <li class="nav-item active">
-        <a class="nav-link" href="{{ route('libros.create')">Crear nuevo Libro</a>
+        <a class="nav-link" href="{{ route('libros.create')}}">Crear nuevo Libro</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#"></a>
